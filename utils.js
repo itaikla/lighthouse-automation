9fc432a22a6edcb89b9a18cb37095483f5acd02e
Itai Klapholtz <itaikla1@gmail.com>
2023-11-07 06:55:42 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,3 +1,24 @@
+export function sendEmail(recipient, subject, body) {
+  try {
+    MailApp.sendEmail({
+      to: recipient,
+      subject: mailSubject,
+      body: body,
+      from: senderEmail,
+    });
+    Logger.log('Email sent successfully to ' + recipient);
+  } catch (error) {
+    Logger.log(`Error sending email: ${error.toString()}`);
+  }
+}
+
+export function appendToExcel(url, row) {
+  var todayDate = new Date().toJSON().slice(0,10).replace(/-/g,'/');
+  const sheet = SpreadsheetApp.openByUrl(url).getSheetByName('Sheet1');
+  var data = [todayDate, row.seoScore, row.accessibilityScore, row.performanceScore, row.bestPracticesScore];
+  sheet.appendRow(data);
+}
+
 export function serializeObject(obj) {
   var str = [];
   for (var p in obj) {
