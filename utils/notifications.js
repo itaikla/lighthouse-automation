f32d438a4b20532085b4115f7bdc6e5fe09719b8
Itai Klapholtz <itaikla1@gmail.com>
2024-01-11 13:53:25 +0200

    Create notifications.js

---
+++ utils/notifications.js
@@ -0,0 +1,23 @@
+export function sendEmail(recipient, subject, body) {
+  try {
+    MailApp.sendEmail({
+      to: recipient,
+      subject: mailSubject,
+      body: body,
+      from: senderEmail,
+    });
+    Logger.log('Email sent successfully to ' + recipient);
+  } catch (error) {
+    Logger.log(`Error sending email: ${error.toString()}`);
+  }
+}
+
+function sendTelegramMessage(message) {
+  const text = encodeURIComponent(message);
+  const url = `${TELEGRAM_API}?chat_id=${CHAT_ID}&text=${text}`;
+  const response = UrlFetchApp.fetch(url, { muteHttpExceptions: true });
+  const { ok, description } = JSON.parse(response);
+  if (ok !== true) {
+    Logger.log(`Error: ${description}`);
+  }
+}
