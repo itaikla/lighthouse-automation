9c8b36abec1049c87ace877d058e0e69026db607
Itai Klapholtz <itaikla1@gmail.com>
2024-01-19 08:13:56 +0200

    Delete utils/utils.ts

--- utils/utils.ts
+++
@@ -1,9 +0,0 @@
-export async function serializeObject(obj: Record<string, string | number | boolean>): Promise<string> {
-  const str: string[] = [];
-  for (const p in obj) {
-    if (obj.hasOwnProperty(p)) {
-      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p].toString()));
-    }
-  }
-  return str.join("&");
-}
